ea16c8108aadc4cc561cdc2457465ce2801e424e
Stefan Kremser <stef.1497@gmail.com>
2018-07-18 12:20:52 +0200

    It works! Don't touch anything!

--- src/ButtonRotaryEncoder.cpp
+++ src/ButtonRotaryEncoder.cpp
@@ -2,55 +2,78 @@
 
 namespace simpleButton {
     ButtonRotaryEncoder::ButtonRotaryEncoder() {
+        this->button_inverted = true;
         enable();
     }
 
     ButtonRotaryEncoder::ButtonRotaryEncoder(uint8_t pin) {
-        this->button_pin = pin;
-        enable();
-    }
-
-    ButtonRotaryEncoder::ButtonRotaryEncoder(Button* button) {
-        this->buttonA = button;
+        this->button_pin      = pin;
+        this->button_inverted = true;
         enable();
     }
 
     ButtonRotaryEncoder::ButtonRotaryEncoder(uint8_t pin, Button* button) {
-        this->button_pin = pin;
-        this->buttonA    = button;
+        this->button_pin      = pin;
+        this->buttonA         = button;
+        this->button_inverted = true;
+        enable();
+    }
+
+    ButtonRotaryEncoder::ButtonRotaryEncoder(uint8_t pin, Button* button, uint8_t button_steps) {
+        this->button_pin      = pin;
+        this->buttonA         = button;
+        this->button_steps    = button_steps;
+        this->button_inverted = true;
         enable();
     }
 
     ButtonRotaryEncoder::~ButtonRotaryEncoder() {}
 
     void ButtonRotaryEncoder::update() {
-        if (button_enabled && button_setup && (millis() - updateTime >= UPDATE_INTERVAL)) {
+        if (button_enabled && button_setup /*&& (millis() - updateTime >= UPDATE_INTERVAL)*/) {
             update(read());
         }
     }
 
     void ButtonRotaryEncoder::update(bool state) {
         if (buttonA) {
+            this->state = state;
             update(buttonA->getState(), state);
         } else {
-            state ? release() : push();
+            this->state = state;
         }
     }
 
-    void ButtonRotaryEncoder::update(bool stateA, bool stateB) {
+    void ButtonRotaryEncoder::update(bool curA, bool curB) {
         updateTime = millis();
 
-        bool curA = stateA;
-        bool curB = stateB;
-
-        if ((prevA == LOW) && (curA == HIGH)) {
-            if (curB == LOW) {
-                click();
-            } else {
-                buttonA->click();
+        if (curState == State::STILL) {
+            if ((curA != prevA) && (curB == prevB)) {
+                prevA    = curA;
+                curState = State::CLOCKWISE;
+            } else if ((curA == prevA) && (curB != prevB)) {
+                prevB    = curB;
+                curState = State::ANTICLOCKWISE;
             }
-        }
+        } else if ((curState != State::STILL) && (curA == curB)) {
+            prevA = curA;
+            prevB = curB;
 
-        prevA = curA;
+            if (curState == prevState) steps++;
+            else steps = 1;
+
+            if (steps >= button_steps) {
+                if (curState == State::CLOCKWISE) {
+                    click();
+                } else /*if (curState == State::ANTICLOCKWISE) */ {
+                    buttonA->click();
+                }
+
+                steps = 0;
+            }
+
+            prevState = curState;
+            curState  = State::STILL;
+        }
     }
 }
--- src/ButtonRotaryEncoder.h
+++ src/ButtonRotaryEncoder.h
@@ -6,24 +6,34 @@
 namespace simpleButton {
     class ButtonRotaryEncoder : public Button {
         public:
-            uint32_t UPDATE_INTERVAL       = 5;
+            uint32_t UPDATE_INTERVAL       = 0;
             uint32_t DEFAULT_MIN_PUSH_TIME = 40;
             uint32_t DEFAULT_TIME_SPAN     = 650;
             uint32_t DEFAULT_HOLD_INTERVAL = 250;
 
             ButtonRotaryEncoder();
             ButtonRotaryEncoder(uint8_t pin);
-            ButtonRotaryEncoder(Button* button);
             ButtonRotaryEncoder(uint8_t pin, Button* button);
+            ButtonRotaryEncoder(uint8_t pin, Button* button, uint8_t button_steps);
+
             ~ButtonRotaryEncoder();
 
             void update();
             void update(bool state);
-            void update(bool stateA, bool stateB);
+            void update(bool curA, bool curB);
 
         private:
             Button* buttonA = NULL;
-            bool prevA      = false;
+
+            bool prevA = false;
+            bool prevB = false;
+
+            enum State { STILL = 0, CLOCKWISE = 1, ANTICLOCKWISE = 2 };
+            State curState  = State::STILL;
+            State prevState = State::STILL;
+
+            uint8_t button_steps = 1;
+            uint8_t steps        = 0;
     };
 }
 
