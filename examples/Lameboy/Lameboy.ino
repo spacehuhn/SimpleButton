b54f6399600c598008526a3c4351c677785d8306
Stefan Kremser <stef.1497@gmail.com>
2018-07-23 14:04:58 +0200

    Reworked Examples

--- examples/Lameboy/Lameboy.ino
+++ examples/Lameboy/Lameboy.ino
@@ -27,13 +27,13 @@ void setup() {
 
     myPCF = new PCF8574(0x3C); // 0x3c = i2c address (use a i2c scanner sketch to find the right address)
 
-    up    = new ButtonPCF(new ButtonPullup(), myPCF, 7);
-    down  = new ButtonPCF(new ButtonPullup(), myPCF, 5);
-    left  = new ButtonPCF(new ButtonPullup(), myPCF, 6);
-    right = new ButtonPCF(new ButtonPullup(), myPCF, 4);
-    a     = new ButtonPCF(new ButtonPullup(), myPCF, 3);
-    b     = new ButtonPCF(new ButtonPullup(), myPCF, 2);
-    sd    = new ButtonPCF(new ButtonPullup(), myPCF, 1);
+    up    = new ButtonPullupPCF(myPCF, 7);
+    down  = new ButtonPullupPCF(myPCF, 5);
+    left  = new ButtonPullupPCF(myPCF, 6);
+    right = new ButtonPullupPCF(myPCF, 4);
+    a     = new ButtonPullupPCF(myPCF, 3);
+    b     = new ButtonPullupPCF(myPCF, 2);
+    sd    = new ButtonPullupPCF(myPCF, 1);
 
     Serial.println("Started");
 }
@@ -48,48 +48,29 @@ void loop() {
     sd->update();
 
     if (up->doubleClicked()) Serial.println("up doubleclicked");
-
     if (up->clicked()) Serial.println("up clicked");
-
     if (up->holding()) Serial.println("up holding");
 
-
     if (down->doubleClicked()) Serial.println("down doubleclicked");
-
     if (down->clicked()) Serial.println("down clicked");
-
     if (down->holding()) Serial.println("down holding");
 
-
     if (left->doubleClicked()) Serial.println("left doubleclicked");
-
     if (left->clicked()) Serial.println("left clicked");
-
     if (left->holding()) Serial.println("left holding");
 
-
     if (right->doubleClicked()) Serial.println("right doubleclicked");
-
     if (right->clicked()) Serial.println("right clicked");
-
     if (right->holding()) Serial.println("right holding");
 
-
     if (a->doubleClicked()) Serial.println("a doubleclicked");
-
     if (a->clicked()) Serial.println("a clicked");
-
     if (a->holding()) Serial.println("a holding");
 
-
     if (b->doubleClicked()) Serial.println("b doubleclicked");
-
     if (b->clicked()) Serial.println("b clicked");
-
     if (b->holding()) Serial.println("b holding");
 
-
     if (sd->pushed()) Serial.println("sd card inserted");
-
     if (sd->released()) Serial.println("sd card removed");
 }
